e4df4a07a66b003b4273b7517b7fa5cbe45ed994
Deborah Lao <dlao7@my.bcit.ca>
2023-11-09 13:42:27 -0800

    Updates breakdown of work document. Adds ability to update and delete reminders.

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -42,10 +42,41 @@ let remindersController = {
 
   update: (req, res) => {
     // implementation here 👈
+    let reminderToFind = req.params.id;
+    remIndex = database.cindy.reminders.findIndex((rem => rem.id == reminderToFind));
+
+    // set boolean in description as true if the status of the radio button was true.
+    if (req.body.completed === "true"){
+      remStatus = true
+    } else {
+      remStatus = false
+    }
+
+    // update the description
+    let updatedReminder = {
+      id: reminderToFind,
+      title: req.body.title,
+      description: req.body.description,
+      completed: remStatus
+    };
+
+    // replace the entry
+    database.cindy.reminders[remIndex] = updatedReminder;
+
+    // Redirect to main reminders page.
+    res.redirect("/reminders");
   },
 
   delete: (req, res) => {
-    // implementation here 👈
+    // Find the index corresponding to the ID of the reminder.
+    let reminderToFind = req.params.id;
+    remIndex = database.cindy.reminders.findIndex((rem => rem.id == reminderToFind));
+    
+    // Remove the reminder at that index.
+    database.cindy.reminders.splice(remIndex, 1);
+
+    // Redirect to main reminders page.
+    res.redirect("/reminders");
   },
 };
 
